e0941aa245944c9f62e13a4208f74fde9391e4f8
Elizabeth Milichenkova <108712381+livecodelove@users.noreply.github.com>
2022-07-06 11:59:46 +0700

    Decoder
    This decoder uses a shift equal to the length of each word

---
+++ main (decoder).py
@@ -0,0 +1,21 @@
+def function():
+    print('This decoder uses a shift equal to the length of each word')
+    words = input('Enter the text you want to decode \n').split()
+    
+    result = ''
+    for i in words:
+       for j in range(len(i)):
+           k = - len(i)
+           if i[j] not in ',.!?:':
+                if i[j] in english_alphabet.upper():
+                    result += english_alphabet[(english_alphabet.upper().index(i[j]) + k) % len(english_alphabet)].upper() 
+                elif  i[j] in english_alphabet:
+                    result += english_alphabet[(english_alphabet.index(i[j]) + k) % len(english_alphabet)]    
+           else:
+               result += i[j]
+       result += ' '
+    return result
+
+
+english_alphabet = 'abcdefghijklmnopqrstuvwxyz'
+print(function())
